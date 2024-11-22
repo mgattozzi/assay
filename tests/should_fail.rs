0313859b075572c302b765d4a3e4b4e70a8a2dbb
Michael Gattozzi <mgattozzi@influxdata.com>
2024-11-22 04:20:28 -0500

    Fix panics and pretty_assert not outputting stdout
    This commit fixes assay so that it actually uses the stdout from the spawned
    subprocess. Up to this point panics would cause tests to fail, however, it did
    not panic with the stdout of that subprocess. It would instead just print out
    the panic message from the callsite in the macro. To rectify this we now install
    a custom panic hook that will use the default if a special header is not passed
    into a panic message. In the replaced hook though we print out the output of the
    panic from the spawned subprocess now. With this we actually now show the proper
    output to the caller, almost as if we never spawned a subprocess.
    
    We now also have a way to test these by using one test to spawn broken tests
    which use the assay macro, but are ignored otherwise. This way we can test bad
    tests or failure cases and check that the output is what we expect!

--- tests/should_fail.rs
+++ tests/should_fail.rs
@@ -8,13 +8,13 @@
 
 //! Inverted tests that we *want* to panic or pass. Given assay causes a test
 //! to spawn itself we need to make sure these tests can actually properly fail.
-//! We run these as part of a script to actually call them. Mainly because these
-//! test that the assay macro works and if a test fails or does not it works as
-//! expected. These are all ignored by default and must be explicitly called for
-//! if we want them to run. This is because we expect these to fail and so we need
-//! to run commands to check for a failure output for CI purposes
+//! We run these with a test that calls cargo test --ignored. This lets us run
+//! them in CI with cargo test --workspace to make sure what would fail is
+//! tested without failing cargo test. These tests are all ignored by default
+//! and must be explicitly called for if we want them to run.
 
 use assay::assay;
+use std::process::Command;
 
 #[assay(ignore)]
 fn should_panic_and_cause_a_failure_case() {
@@ -23,3 +23,30 @@ fn should_panic_and_cause_a_failure_case() {
 
 #[assay(ignore, should_panic)]
 fn should_not_panic_and_cause_a_failure_case() {}
+
+#[test]
+fn panics_in_macros() {
+  let output = Command::new("cargo")
+    .args([
+      "test",
+      "--workspace",
+      "--",
+      "--ignored",
+      "panic_and_cause_a_failure_case",
+    ])
+    .output()
+    .unwrap();
+  let tests = String::from_utf8(output.stdout).unwrap();
+
+  if !tests.contains(
+    "---- should_not_panic_and_cause_a_failure_case stdout ----
+note: test did not panic as expected",
+  ) && !tests.contains(
+    "---- should_panic_and_cause_a_failure_case stdout ----
+thread 'should_panic_and_cause_a_failure_case' panicked at tests/should_fail.rs:21:3:
+explicit panic
+note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace",
+  ) {
+    panic!("Unexpected output for panics.\n\nOutput:\n{}", tests);
+  }
+}
